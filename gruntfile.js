da5d45167fbe1e4d046f76b92acc528ace66f797
Zhao ZHANG <zo.zhang@gmail.com>
2021-01-28 18:41:55 +0100

    Update Grunt Css Script

--- gruntfile.js
+++ gruntfile.js
@@ -1,10 +1,13 @@
+const sass = require('node-sass');
+
 module.exports = function (grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     sass: {
       options: {
-        sourceMap: true
+        implementation: sass,
+        sourceMap: true,
       },
       dist: {
         files: {
